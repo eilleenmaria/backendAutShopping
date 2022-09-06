1ffa4554e450768ef83d6d94c5bc862274316f68
eilleenmaria <egutierrezacosta85@gmail.com>
2022-09-05 21:42:05 -0500

    config cors index

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express =require ('express');
 const conectDB = require ('./config/db');
-
+const { populate } = require('./models/Car');
+const cors = require ('cors');
 
 // we create the server
 const app = express();
 // we connect the server
 conectDB();
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/car', require('./routes/car'));
